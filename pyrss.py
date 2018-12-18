d2d8c6c1ccc8940d94caf86fff458d7effa8fff6
spawndemonic <spawndemonic@gmail.com>
2018-12-18 15:44:38 -0500

    Create pyrss.py

---
+++ pyrss.py
@@ -0,0 +1,33 @@
+import feedparser
+import time
+
+class NODE:
+   def __init__(self, title, link, pubDate, desc):
+    self.title = title
+    self.link = link
+    self.pubDate = pubDate
+    self.desc = desc
+    self.lifeT = 
+    self.next = None
+class feed:
+  def __init__(self):
+    self.head = None
+# Functions:
+# Read sources from txt file
+# Go through each source to fill feed
+# Check periodically for old articles
+def displayFeed():
+  start = feed()
+  if start.head == None:
+    #start list
+  else:
+    # go through list check for existing articles
+    # if article is not in list add to list
+    # display list format: title:desc:pubdate  
+  
+
+uInput = ''
+while uInput !='q':
+  displayFeed()
+  
+  
