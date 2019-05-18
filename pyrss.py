830454d21d0387cb5ce2525903476b1d482d4c54
Spawndemonic <spawndemonic@gmail.com>
2019-05-18 12:03:00 -0400

    Minor fixes

--- pyrss.py
+++ pyrss.py
@@ -11,13 +11,9 @@ class NODE:
     self.desc = desc
     self.next = None
       
-class feed:
-  def __init__(self):
-    self.head = None
-      
 # Functions:
 # Read sources from txt file
-def gatherSources(source)
+def gatherSources(source):
   with open(filename,"r") as f:
      for line in f:
         source.append(line)
@@ -31,29 +27,30 @@ def createFeed(sources,head):
       e = feedparser.parse(k)
       for e in range(len(k.entries)):
          addArticle(k.entries[e],start)
-  else:
+ # else:
     # go through list check for existing articles
     # if article is not in list add to list
     # display list format: title:desc:pubdate  
   
 # Check periodically for old articles
 def displayFeed(head):
-  display = head
-  while display.next != Null:
-    for i in range(len(display.entries)):
-      print display.title
-      print display.desc
-      print display.pubDate
-      display = display.next
+    display = head
+    while display.next != Null:
+        for i in range(len(display.entries)):
+            print display.title
+            print display.desc
+            print display.pubDate
+            display = display.next
          
   
 if len(sys.argv) >1:
    writer = open(filename,"w")
    writer.write(sys.argv[1])
    writer.close()
-   
-   
+    
+uInput = 'a'
+start = NODE()
 while uInput !='q':
-  displayFeed()
-  
+  displayFeed(start)
+  uInput = input()    
   
